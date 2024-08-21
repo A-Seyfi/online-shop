95beb71b9a48f9208ad2ee8897ff29e72baf63fd
A-Seyfi <seyfiabolfazl64@gmail.com>
2024-08-21 21:08:06 +0330

    created user basket page

--- static/js/price-range.js
+++ static/js/price-range.js
@@ -1,22 +1,3 @@
-/* =========================================================
- * bootstrap-slider.js v2.0.0
- * http://www.eyecon.ro/bootstrap-slider
- * =========================================================
- * Copyright 2012 Stefan Petre
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * ========================================================= */
- 
 !function( $ ) {
 
 	var Slider = function(element, options) {
@@ -154,8 +135,6 @@
 		
 		showTooltip: function(){
 			this.tooltip.addClass('in');
-			//var left = Math.round(this.percent*this.width);
-			//this.tooltip.css('left', left - this.tooltip.outerWidth()/2);
 			this.over = true;
 		},
 		
--- user_panel_module/forms.py
+++ user_panel_module/forms.py
@@ -8,15 +8,12 @@ from account_module.models import User
 class EditProfileModelForm(forms.ModelForm):
     class Meta:
         model = User
-        fields = ['first_name', 'last_name', 'avatar', 'address', 'about_user']
+        fields = ['first_name', 'last_name', 'address', 'about_user']
         widgets = {
             'first_name': forms.TextInput(attrs={
                 'class': 'form-control'
             }),
             'last_name': forms.TextInput(attrs={
-                'class': 'form-control'
-            }),
-            'avatar': forms.FileInput(attrs={
                 'class': 'form-control'
             }),
             'address': forms.Textarea(attrs={
@@ -33,7 +30,6 @@ class EditProfileModelForm(forms.ModelForm):
         labels = {
             'first_name': 'نام',
             'last_name': 'نام خانوادگی',
-            'avatar': 'تصویر پروفایل',
             'address': 'آدرس',
             'about_user': 'درباره شخص',
         }
